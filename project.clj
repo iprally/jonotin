69aedec8edc406b09a65b9d75d1215ee9310ecde
cez <juuso@iprally.com>
2021-11-26 15:42:54 +0200

    Version 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jonotin "0.3.0"
+(defproject jonotin "0.3.1"
   :description "Google Pub/Sub Java SDK wrapper"
   :url "https://github.com/iprally/jonotin"
   :license {:name "The MIT License"
