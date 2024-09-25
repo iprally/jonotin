2c100f23d073325a52e86824522b3f5ae8ffed6b
cez <juuso@iprally.com>
2024-09-25 14:21:54 +0300

    Dependency update

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.google.cloud/google-cloud-pubsub "1.126.6"]]
+  :dependencies [[org.clojure/clojure "1.11.4"]
+                 [com.google.cloud/google-cloud-pubsub "1.132.2"]]
   :source-paths ["src"])
